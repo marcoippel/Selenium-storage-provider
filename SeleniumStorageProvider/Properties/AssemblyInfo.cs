fa3d8f081d5445aa14d86dc52973df6b815dfba8
Marco Ippel <marco@ippelsoft.nl>
2015-08-11 21:44:21 +0200

    Release version 10.0.0.8

--- SeleniumStorageProvider/Properties/AssemblyInfo.cs
+++ SeleniumStorageProvider/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SeleniumStorageProvider")]
+[assembly: AssemblyTitle("Selenium screenshot storage provider")]
 [assembly: AssemblyDescription("Provider to store selenium screenshots in data storage providers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Marco Ippel")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.7")]
-[assembly: AssemblyFileVersion("1.0.0.7")]
+[assembly: AssemblyVersion("1.0.0.8")]
+[assembly: AssemblyFileVersion("1.0.0.8")]
